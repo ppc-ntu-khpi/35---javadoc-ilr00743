23634e12a57ded4dffd977bc5d8c2d5f55c5c02b
Illia_ryzhiy <ilr00743@gmail.com>
2021-01-14 19:21:03 +0200

    Add files via upload

--- src/domain/GraphicIllustrator.java
+++ src/domain/GraphicIllustrator.java
@@ -1,3 +1,9 @@
 package domain;
+
+/**
+ * A subclass of Artist that represent an graphical illustrator
+ * @see Artist
+ * @author Illia_R
+ */
 public class GraphicIllustrator extends Artist {
 }
--- src/domain/TechnicalWriter.java
+++ src/domain/TechnicalWriter.java
@@ -1,3 +1,9 @@
 package domain;
+
+/**
+ * A subclass of Artist that represent a technical writer
+ * @see Artist
+ * @author Illia_R
+ */
 public class TechnicalWriter extends Artist {
 }
